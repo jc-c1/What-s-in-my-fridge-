d8aaa8243e76793d07b9b6b938bf3023bc52eadc
jc-c1 <joannec.chang1@gmail.com>
2024-02-21 23:42:02 -0800

    specified Gemini prompt to generate response in html code that starts from the <p> tag, added parse from html-react-parser to parse it and added some div to stylize a little so the text is centered

--- learn/src/components/recipeGeneration.js
+++ learn/src/components/recipeGeneration.js
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import parse from 'html-react-parser';
 
 
 const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
@@ -10,8 +11,7 @@ export const GeminiComponent = ({ ing }) => {
         i.quantity.toString() + " " + i.unit + "(s) of " + i.name
     )).join(", ")
 
-    const prompt = "Can you please make give me a recipe that uses " + ingList + "?"
-
+    const prompt = "Can you please make give me a beginner-friendly recipe that uses " + ingList + "? Please only include only the name of the dish, the ingredients, and the instructions. Can you also format the response with html tags starting from the <p> tag?"
 
     // const [prompt, setPrompt] = useState("");
     const [generatedText, setGeneratedText] = useState("");
@@ -23,18 +23,28 @@ export const GeminiComponent = ({ ing }) => {
             const text = response.text();
             console.log(text)
             // Update the UI with the generated response
+
             setGeneratedText(text);
         } catch (error) {
             console.error("Error generating text:", error);
             // Handle errors appropriately
-            
+
         }
     };
+
+
 
     return (
         <div>
             <button onClick={() => handleGenerate(prompt)}>Generate</button>
-            <p>{generatedText}</p>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+
+                <div style={{ textAlign: "left" }}>{parse(generatedText)}</div>
+            </div>
         </div>
     );
 }
